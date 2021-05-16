10b9d464ace478412504984336c1b419755b1361
sinchang <sinchangwen@gmail.com>
2021-05-16 15:04:39 +0800

    chore: update example

--- example/index.js
+++ example/index.js
@@ -1,15 +1,41 @@
-import qrcodeParser from '../src'
+import qrcodeParser from "../src";
 
-const imageUrl = 'https://i.imgur.com/2S3N16R.png'
+const fileElement = document.getElementById("file");
+fileElement.addEventListener(
+  "change",
+  () => {
+    const file = fileElement.files[0];
+    qrcodeParser(file).then(res => {
+      document.getElementById("content3").innerText = res.data;
+    });
+  },
+  false
+);
 
-qrcodeParser(imageUrl).then(res => {
-  document.getElementById('content').innerText = res.data
-})
+document.getElementById("parse-image-url").addEventListener(
+  "click",
+  () => {
+    const url = document.getElementById("image-url").value;
 
-const fileElement = document.getElementById('file')
-fileElement.addEventListener('change', () => {
-  const file = fileElement.files[0]
-  qrcodeParser(file).then(res => {
-    document.getElementById('content').innerText = res.data
-  })
-}, false)
+    if (!url) return;
+
+    qrcodeParser(url).then(res => {
+      document.getElementById("content1").innerText = res.data;
+    });
+  },
+  false
+);
+
+document.getElementById("parse-image-base64").addEventListener(
+  "click",
+  () => {
+    const url = document.getElementById("image-base64").value;
+
+    if (!url) return;
+
+    qrcodeParser(url).then(res => {
+      document.getElementById("content2").innerText = res.data;
+    });
+  },
+  false
+);
